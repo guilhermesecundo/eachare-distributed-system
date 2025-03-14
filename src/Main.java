6426a0dad916e2c67045ad52a4db584c40c61af1
Luis Yudi <luisyudi1@gmail.com>
2025-03-14 13:56:33 -0300

    Initial definitions

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+package src;
+
 public class Main {
     private static String address; 
     private static String port;
     private static String neighbors; // possivelmente mudar 
-    private static String folder;    // possivelmente mudar  
+    private static String folder;    // possivelmente mudar
+    private static int clock;
 
     public static void main(String[] args) {
         //Depois fazer as verificacoes necessarias
@@ -11,6 +14,12 @@ public class Main {
         neighbors = args[2];
         folder = args[3];
 
-        
+        //Instancia objeto peer()
+
+        //TODO: Criar socket tcp
+        //TODO: criar duas threads a de ouvir e enviar
+
+        //Run()
+        //Run()
     }
 }
