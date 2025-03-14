f4a74623c41d745cf415012bd73f358d84ebbc4e
Luis Yudi <luisyudi1@gmail.com>
2025-03-14 13:09:52 -0300

    initial

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    private static String address; 
+    private static String port;
+    private static String neighbors; // possivelmente mudar 
+    private static String folder;    // possivelmente mudar  
+
+    public static void main(String[] args) {
+        //Depois fazer as verificacoes necessarias
+        address = args[0];
+        port = args[1];
+        neighbors = args[2];
+        folder = args[3];
+
+        
+    }
+}
